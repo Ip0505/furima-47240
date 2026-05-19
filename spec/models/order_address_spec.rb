92d0abef3ee30ec5d9c25f07a58021e6d1381c8d
Ip0505 <yokimura1234@yahoo.ne.jp>
2026-05-19 11:32:55 +0900

    商品購入機能 #10

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -2,9 +2,14 @@ require 'rails_helper'
 
 RSpec.describe OrderAddress, type: :model do
   before do
-    @order_address = FactoryBot.build(:order_address)
-    @order_address.user_id = 1
-    @order_address.item_id = 1
+    @user = FactoryBot.create(:user)
+    @item = FactoryBot.create(:item, user: @user)
+
+    @order_address = FactoryBot.build(
+      :order_address,
+      user_id: @user.id,
+      item_id: @item.id
+    )
   end
 
   describe '商品購入機能' do
