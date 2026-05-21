2c6491a2439b5bcb91cd473b24dae2ef4ef54b84
Ip0505 <yokimura1234@yahoo.ne.jp>
2026-05-21 09:53:24 +0900

    不具合修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -2,15 +2,21 @@ const price = () => {
   const priceInput = document.getElementById("item-price")
   if (!priceInput) return
 
+  const addTaxPrice = document.getElementById("add-tax-price")
+  const profit = document.getElementById("profit")
+  if (!addTaxPrice || !profit) return
+
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value
-    const addTaxPrice = document.getElementById("add-tax-price")
-    const profit = document.getElementById("profit")
 
-    if (!addTaxPrice || !profit) return
+    if (inputValue === "") {
+      addTaxPrice.innerHTML = ""
+      profit.innerHTML = ""
+      return
+    }
 
-    const tax = Math.floor(inputValue * 0.1)
-    const salesProfit = Math.floor(inputValue - tax)
+    const tax = Math.floor(Number(inputValue) * 0.1)
+    const salesProfit = Number(inputValue) - tax
 
     addTaxPrice.innerHTML = tax
     profit.innerHTML = salesProfit
@@ -18,3 +24,4 @@ const price = () => {
 }
 
 window.addEventListener("turbo:load", price)
+window.addEventListener("turbo:render", price)
